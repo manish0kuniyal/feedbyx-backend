aa41863fbed85b07df96e13a8b95812919447125
manish0kuniyal <manishkuniyal24@gmail.com>
2026-02-01 14:06:50 +0530

    lambdaerror

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({ message: '...Express live 🔥🔥 ' });
+  res.json({ message: '...Express live 🔥🔥🔥 ' });
 });
 
 // Routes
