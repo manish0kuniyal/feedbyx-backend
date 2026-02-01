1f381530aa5328cdb5be7f0da42024ff2f624f5f
manish0kuniyal <manishkuniyal24@gmail.com>
2026-02-01 13:45:19 +0530

    lambdaerror

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
   if (origin && ALLOWED_ORIGINS.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials");
+    res.setHeader("Access-Control-Allow-Credentials","true");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization"
